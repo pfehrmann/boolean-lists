41cffba5958083ea2b98013bf9039965428b52e8
Philipp Fehrmann <philipp-09@live.de>
2018-05-27 17:48:56 +0200

    Add spotify api

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,4 +1,18 @@
-function hello(compiler: string) {
-    console.log(`Hello from ${compiler}`);
-}
-hello("TypeScript");
+let env = require('dotenv').config();
+let SpotifyWebApi = require('spotify-web-api-node');
+let opn = require('opn');
+
+var spotifyApi = new SpotifyWebApi({
+  clientId: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET,
+  redirectUri: process.env.REDIRECT_URI,
+});
+
+let scopes = ['user-read-private', 'user-read-email'];
+let state = 'random-state'
+
+let authorizeURL = spotifyApi.createAuthorizeURL(scopes, state);
+
+console.log(authorizeURL);
+
+opn(authorizeURL);
