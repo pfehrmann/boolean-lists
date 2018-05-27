5e612cc25877c0792183338db5ff11b0bfc3bcee
Philipp Fehrmann <philipp-09@live.de>
2018-05-27 17:45:41 +0200

    Add monitoring of sources

--- backend/gulpfile.js
+++ backend/gulpfile.js
@@ -2,7 +2,11 @@ var gulp = require("gulp");
 var ts = require("gulp-typescript");
 var tsProject = ts.createProject("tsconfig.json");
 
-gulp.task("default", function () {
+gulp.task("default", function() {
+  return gulp.watch("src/**/*", ["build"]);
+})
+
+gulp.task("build", function () {
     return tsProject.src()
         .pipe(tsProject())
         .js.pipe(gulp.dest("dist"));
