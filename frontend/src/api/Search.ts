01a13914a0544ed1128b49eef7fd1b551c088385
Philipp Fehrmann <philipp-09@live.de>
2018-09-02 20:05:26 +0200

    Add wrapper for search api

---
+++ frontend/src/api/Search.ts
@@ -0,0 +1,15 @@
+export async function searchPlaylist(name: string) {
+    const untypedPlaylists = await (await fetch(`http://localhost:3000/search/playlist?q=${ name }`)).json();
+    return untypedPlaylists.map((playlist: any) => {
+        return {
+            id: playlist.id,
+            image: {
+                height: playlist.image.height,
+                url: playlist.image.url,
+                width: playlist.image.width
+            },
+            name: playlist.name(),
+            userId: playlist.userId()
+        }
+    });
+}
