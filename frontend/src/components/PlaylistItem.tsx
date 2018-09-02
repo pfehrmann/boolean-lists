fcef0ea8c4b4ad3bcc7b356f7aef5ee345f7c2e4
Philipp Fehrmann <philipp-09@live.de>
2018-09-02 22:18:47 +0200

    Add dialog for selecting playlist

---
+++ frontend/src/components/PlaylistItem.tsx
@@ -0,0 +1,37 @@
+import Avatar from '@material-ui/core/Avatar';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import * as React from "react";
+
+interface IPlaylistProps {
+    playlist: {
+        name: string;
+        image: {
+            url: string;
+        }
+        userId: string;
+        id: string;
+    }
+    handleClose: (selection: any) => any;
+}
+
+export class PlaylistItem extends React.Component<IPlaylistProps> {
+    constructor(props: IPlaylistProps) {
+        super(props);
+        this.state = {};
+
+        this.close = this.close.bind(this);
+    }
+
+    public render() {
+        return (<ListItem button={true} onClick={this.close}>
+            <Avatar alt={this.props.playlist.name} src={this.props.playlist.image.url}/>
+            <ListItemText primary={this.props.playlist.name}/>
+
+        </ListItem>);
+    }
+
+    private close() {
+        this.props.handleClose(this.props.playlist);
+    }
+}
--- frontend/src/nodes/PlaylistNodeWidget.tsx
+++ frontend/src/nodes/PlaylistNodeWidget.tsx
@@ -1,5 +1,17 @@
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import TextField from '@material-ui/core/TextField';
+import * as _ from "lodash";
+import * as React from "react";
 import * as SRD from "storm-react-diagrams";
+import * as logger from "winston";
+import * as Search from "../api/Search";
 
+import {PlaylistItem} from "../components/PlaylistItem";
 import {AbstractNodeWidget, IAbstractNodeProps} from "./AbstractNodeWidget";
 import PlaylistNodeModel from "./PlaylistNodeModel";
 
@@ -8,13 +20,109 @@ export interface IPlaylistNodeProps extends IAbstractNodeProps<PlaylistNodeModel
     diagramEngine: SRD.DiagramEngine;
 }
 
+interface IPlaylistNodeState {
+    configOpen: boolean;
+    playlistItems: React.Component[],
+    playlistResults: any[];
+    searchQuery: string;
+}
+
 export default class PlaylistNodeWidget extends AbstractNodeWidget<IPlaylistNodeProps> {
+    public state: IPlaylistNodeState;
+
     constructor(props: IPlaylistNodeProps) {
         super("playlist-node", props);
-        this.state = {};
+
+        this.state = {
+            configOpen: false,
+            playlistItems: [],
+            playlistResults: [],
+            searchQuery: ""
+        };
+
+        this.render = this.render.bind(this);
+        this.onDoubleClick = this.onDoubleClick.bind(this);
+        this.handleClose = this.handleClose.bind(this);
+        this.handleKeypress = this.handleKeypress.bind(this);
+        this.updateSearchQuery = this.updateSearchQuery.bind(this);
+        this.handleSelect = this.handleSelect.bind(this);
+    }
+
+    private static stopPropagation(event: any) {
+        event.stopPropagation();
+    }
+
+    public render() {
+        return (<div>
+            {super.render()}
+            <Dialog onClose={this.handleClose} aria-labelledby="simple-dialog-title" open={this.state.configOpen}
+                    onKeyUp={PlaylistNodeWidget.stopPropagation} onDrag={PlaylistNodeWidget.stopPropagation}
+                    onScroll={PlaylistNodeWidget.stopPropagation}>
+                <DialogTitle id="simple-dialog-title">Search for a playlist</DialogTitle>
+                <DialogContent>
+                    <DialogContentText>
+                        Find a Spotify playlist you like for this node.
+                    </DialogContentText>
+                    <TextField
+                        autoFocus={true}
+                        margin="dense"
+                        id="query"
+                        label="Playlist name"
+                        type="search"
+                        fullWidth={true}
+                        value={this.state.searchQuery}
+                        onChange={this.updateSearchQuery}
+                        onKeyUp={_.throttle(this.handleKeypress, 250)}
+                    />
+                    {this.state.playlistItems}
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={this.handleClose} color="primary">
+                        Cancel
+                    </Button>
+                    <Button onClick={this.handleClose} color="primary">
+                        Save
+                    </Button>
+                </DialogActions>
+            </Dialog>
+        </div>)
     }
 
     public onDoubleClick() {
-        alert("Double Clicked");
+        logger.info("Clicked.");
+        this.setState({
+            configOpen: true
+        });
+    }
+
+    private updateSearchQuery(evt: any) {
+        this.setState({
+            searchQuery: evt.target.value
+        });
+    }
+
+    private async handleKeypress() {
+        const playlists = await Search.searchPlaylist(this.state.searchQuery);
+        const playlistItems = playlists.map((playlist: any) => <PlaylistItem key={playlist.id}
+                                                                             handleClose={this.handleSelect}
+                                                                             playlist={...playlist}/>);
+
+        this.setState({
+            playlistItems,
+            playlistResults: playlists
+        });
+    }
+
+    private handleSelect(playlist: any) {
+        this.props.node.configuration.id = playlist.id;
+        this.props.node.configuration.userId = playlist.userId;
+        this.props.node.name = `Playlist '${playlist.name}'`;
+        this.handleClose();
+    }
+
+    private handleClose() {
+        this.setState({
+            configOpen: false
+        });
     }
 }
