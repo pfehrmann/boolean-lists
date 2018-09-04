3445d93cbf545d8c57b75c9ee05c1c824afb48ba
Philipp Fehrmann <philipp-09@live.de>
2018-09-04 21:21:56 +0200

    Set default options

--- frontend/src/Graph.tsx
+++ frontend/src/Graph.tsx
@@ -9,7 +9,8 @@ class Graph extends React.Component<{ engine: SRD.DiagramEngine }> {
 
     public render() {
         return (
-            <SRD.DiagramWidget diagramEngine={this.props.engine} className="graph"/>
+            <SRD.DiagramWidget diagramEngine={this.props.engine} allowLooseLinks={false} maxNumberPointsPerLink={0}
+                               className="graph"/>
         );
     }
 }
