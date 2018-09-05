30e87d027f903d164525774d908756f8d629b801
Philipp Fehrmann <philipp-09@live.de>
2018-09-05 20:50:38 +0200

    Add tslint to gulp

--- backend/gulpfile.js
+++ backend/gulpfile.js
@@ -1,10 +1,24 @@
 var gulp = require("gulp");
 var ts = require("gulp-typescript");
+var tslint = require("gulp-tslint");
+
 var tsProject = ts.createProject("tsconfig.json");
 
-gulp.task("default", function() {
-  return gulp.watch("src/**/*", ["build"]);
+gulp.task("default", ["tslint", "build", "watch"]);
+
+gulp.task("watch", () => {
+  return gulp.watch("src/**/*", ["tslint", "build"]);
 })
+
+gulp.task("tslint", () => {
+  gulp.src("src/**/*.ts")
+        .pipe(tslint({
+            formatter: "stylish"
+        }))
+        .pipe(tslint.report({
+           emitError: false
+        }))
+});
 
 gulp.task("build", function () {
     return tsProject.src()
