8ae5adb7a4a2fc4d453d84735167a0187d79daa4
Philipp Fehrmann <philipp-09@live.de>
2018-09-11 23:03:26 +0200

    Use menu for adding components

--- frontend/src/components/AddNodesElement.tsx
+++ frontend/src/components/AddNodesElement.tsx
@@ -1,6 +1,10 @@
 import Button from '@material-ui/core/Button';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+import AddIcon from '@material-ui/icons/Add';
 import * as React from "react";
 import * as SRD from "storm-react-diagrams";
+
 import AddNodeModel from "../nodes/AddNode/AddNodeModel";
 import AlbumNodeModel from "../nodes/AlbumNode/AlbumNodeModel";
 import LimitNodeModel from "../nodes/LimitNode/LimitNodeModel";
@@ -14,10 +18,18 @@ interface IAddNodesElementProps {
     model: SRD.DiagramModel;
 }
 
+interface IAddNodesElementState {
+    anchorEl: any;
+}
+
 export class AddNodesElement extends React.Component<IAddNodesElementProps> {
+    public state: IAddNodesElementState;
+
     constructor(props: IAddNodesElementProps) {
         super(props);
-        this.state = {};
+        this.state = {
+            anchorEl: undefined
+        };
 
         this.addPlaylistNode = this.addPlaylistNode.bind(this);
         this.addAddNode = this.addAddNode.bind(this);
@@ -26,22 +38,52 @@ export class AddNodesElement extends React.Component<IAddNodesElementProps> {
         this.addSubtractNode = this.addSubtractNode.bind(this);
         this.addMyTopTracksNode = this.addMyTopTracksNode.bind(this);
         this.addAlbumNode = this.addAlbumNode.bind(this);
+
+        this.handleClick = this.handleClick.bind(this);
+        this.handleClose = this.handleClose.bind(this);
     }
 
     public render() {
         return (
             <div>
-                <Button variant="contained" color="primary" onClick={this.addPlaylistNode}>Add a PlaylistNode</Button>
-                <Button variant="contained" color="primary" onClick={this.addAddNode}>Add an AddNode</Button>
-                <Button variant="contained" color="primary" onClick={this.addSubtractNode}>Add a SubtractNode</Button>
-                <Button variant="contained" color="primary" onClick={this.addLimitNode}>Add a LimitNode</Button>
-                <Button variant="contained" color="primary" onClick={this.addRandomizeNode}>Add a RandomizeNode</Button>
-                <Button variant="contained" color="primary" onClick={this.addMyTopTracksNode}>Add a MyTopTracksNode</Button>
-                <Button variant="contained" color="primary" onClick={this.addAlbumNode}>Add a AlbumNode</Button>
-            </div>);
+                <Button
+                    variant="fab"
+                    color="primary"
+                    aria-label="Add"
+                    aria-owns={this.state.anchorEl ? "add-node-menu" : undefined}
+                    aria-haspopup={true}
+                    onClick={this.handleClick}
+                >
+                    <AddIcon/>
+                </Button>
+                <Menu
+                    id={"add-node-menu"}
+                    anchorEl={this.state.anchorEl}
+                    open={Boolean(this.state.anchorEl)}
+                    onClose={this.handleClose}
+                >
+                    <MenuItem onClick={this.addPlaylistNode}>Add a PlaylistNode</MenuItem>
+                    <MenuItem onClick={this.addAddNode}>Add an AddNode</MenuItem>
+                    <MenuItem onClick={this.addSubtractNode}>Add a SubtractNode</MenuItem>
+                    <MenuItem onClick={this.addLimitNode}>Add a LimitNode</MenuItem>
+                    <MenuItem onClick={this.addRandomizeNode}>Add a RandomizeNode</MenuItem>
+                    <MenuItem onClick={this.addMyTopTracksNode}>Add a MyTopTracksNode</MenuItem>
+                    <MenuItem onClick={this.addAlbumNode}>Add a AlbumNode</MenuItem>
+                </Menu>
+            </div>
+        );
     }
 
+    public handleClick(event: any) {
+        this.setState({ anchorEl: event.currentTarget });
+    };
+
+    public handleClose() {
+        this.setState({ anchorEl: undefined });
+    };
+
     public addPlaylistNode() {
+        this.handleClose();
         const node = PlaylistNodeModel.getInstance();
         node.setPosition(0, 0);
 
@@ -50,6 +92,7 @@ export class AddNodesElement extends React.Component<IAddNodesElementProps> {
     }
 
     public addAddNode() {
+        this.handleClose();
         const node = AddNodeModel.getInstance();
         node.setPosition(50, 10);
 
@@ -58,6 +101,7 @@ export class AddNodesElement extends React.Component<IAddNodesElementProps> {
     }
 
     public addSubtractNode() {
+        this.handleClose();
         const node = SubtractNodeModel.getInstance();
         node.setPosition(50, 10);
 
@@ -66,6 +110,7 @@ export class AddNodesElement extends React.Component<IAddNodesElementProps> {
     }
 
     public addLimitNode() {
+        this.handleClose();
         const node = LimitNodeModel.getInstance();
         node.setPosition(50, 10);
 
@@ -74,6 +119,7 @@ export class AddNodesElement extends React.Component<IAddNodesElementProps> {
     }
 
     public addRandomizeNode() {
+        this.handleClose();
         const node = RandomizeNodeModel.getInstance();
         node.setPosition(50, 10);
 
@@ -82,6 +128,7 @@ export class AddNodesElement extends React.Component<IAddNodesElementProps> {
     }
 
     public addMyTopTracksNode() {
+        this.handleClose();
         const node = MyTopTracksNodeModel.getInstance();
         node.setPosition(50, 10);
 
@@ -90,6 +137,7 @@ export class AddNodesElement extends React.Component<IAddNodesElementProps> {
     }
 
     public addAlbumNode() {
+        this.handleClose();
         const node = AlbumNodeModel.getInstance();
         node.setPosition(50, 10);
 
