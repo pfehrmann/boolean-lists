777d74435e34d2e5fe3eea22721543ed577265cb
Philipp Fehrmann <philipp-09@live.de>
2018-10-28 20:15:52 +0100

    Update landing

--- frontend/src/pages/Landing.tsx
+++ frontend/src/pages/Landing.tsx
@@ -31,46 +31,73 @@ class Landing extends React.Component<{ classes: any }> {
         return (
             <div className={this.props.classes.base}>
                 <Parallax bgImage={concert} strength={500}>
-                    <div style={{height: 500}}>
+                    <div style={{minHeight: 400}}>
                         <Grid container={true} style={{marginTop: "50px"}} justify={"center"}>
                             <Grid item={true} xs={11} md={8}>
                                 <Typography variant="display4">BooleanLists</Typography>
-                                <Typography variant="display3">Supercharge your Spotify playists with
+                                <Typography variant="display3">Supercharge your Spotify playlists with
                                     ease!</Typography>
                             </Grid>
                         </Grid>
                     </div>
                 </Parallax>
-                <Parallax bgImage={turntables} strength={500}>
-                <Grid container={true} style={{marginTop: "20px", maxWidth: "95vw"}} justify={"center"} spacing={24}>
-                    <Grid item={true} xs={8} lg={3}>
-                        <Card>
-                            <CardContent>
-                                <Typography variant="display2">Easy to use</Typography><br/>
-                                <Typography variant="display1">We have created a super simple to use graph and node
-                                    based editor to make editing a breeze</Typography>
-                            </CardContent>
-                        </Card>
+                <Parallax bgImage={turntables} strength={500} style={{padding: 24}}>
+                    <Grid
+                        container={true}
+                        justify={"center"}
+                        spacing={24}
+                    >
+                        <Grid item={true} xs={8} lg={4}>
+                            <Card>
+                                <CardContent>
+                                    <Typography variant="display2">Easy to use</Typography><br/>
+                                    <Typography variant="display1">We have created a super simple to use graph and node
+                                        based editor to make editing a breeze</Typography>
+                                </CardContent>
+                            </Card>
+                        </Grid>
+                        <Grid item={true} xs={8} lg={4}>
+                            <Card>
+                                <CardContent>
+                                    <Typography variant="display2">Store your lists</Typography><br/>
+                                    <Typography variant="display1">Yeah, we know, it's a pretty basic feature. But
+                                        whatever.
+                                        You can do it :D</Typography>
+                                </CardContent>
+                            </Card>
+                        </Grid>
+                        <Grid item={true} xs={8} lg={4}>
+                            <Card>
+                                <CardContent>
+                                    <Typography variant="display2">Free</Typography><br/>
+                                    <Typography variant="display1">Yep, you have heard right: This service is completely
+                                        free, so no worries about any costs.</Typography>
+                                </CardContent>
+                            </Card>
+                        </Grid>
+                        <Grid item={true} xs={8} lg={4}>
+                            <Card>
+                                <CardContent>
+                                    <Typography variant="display2">Beta</Typography><br/>
+                                    <Typography variant="display1">This project is absolutely not done by now, so your
+                                        data
+                                        might be lost, playlists may not synchronize correctly, and so on. Be aware of
+                                        this.
+                                    </Typography>
+                                </CardContent>
+                            </Card>
+                        </Grid>
+                        <Grid item={true} xs={8} lg={4}>
+                            <Card>
+                                <CardContent>
+                                    <Typography variant="display2">Questions?</Typography><br/>
+                                    <Typography variant="display1">In case you have any questions, feel free to write an
+                                        email to <a href={"mailto:philipp.fehrmann@gmx.de"}>philipp.fehrmann@gmx.de</a>
+                                    </Typography>
+                                </CardContent>
+                            </Card>
+                        </Grid>
                     </Grid>
-                    <Grid item={true} xs={8} lg={3}>
-                        <Card>
-                            <CardContent>
-                                <Typography variant="display2">Store your lists</Typography><br/>
-                                <Typography variant="display1">Yeah, we know, it's a pretty basic feature. But whatever.
-                                    You can do it :D</Typography>
-                            </CardContent>
-                        </Card>
-                    </Grid>
-                    <Grid item={true} xs={8} lg={3}>
-                        <Card>
-                            <CardContent>
-                                <Typography variant="display2">Free</Typography><br/>
-                                <Typography variant="display1">Yep, you have heart right: This service is completly
-                                    free, so no worries about any costs.</Typography>
-                            </CardContent>
-                        </Card>
-                    </Grid>
-                </Grid>
                 </Parallax>
             </div>
         );
