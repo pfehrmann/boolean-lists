f00547b5e204d288af6199435ec51c4e99e1c01d
Philipp Fehrmann <philipp-09@live.de>
2018-11-04 22:58:53 +0100

    Add logging info

--- backend/src/api/Playlists.ts
+++ backend/src/api/Playlists.ts
@@ -1,10 +1,12 @@
 import * as Express from "express";
+import * as logger from "winston";
 import {Playlist} from "../model/spotify/Playlist";
 import {InitializedSpotifyApi} from "../model/spotify/SpotifyApi";
 
 const router: Express.Router = Express.Router();
 
 router.get("/", async (req, res) => {
+    logger.info("Get api from request...");
     const api = new InitializedSpotifyApi((req as any).api);
     try {
         if (req.query.uri) {
