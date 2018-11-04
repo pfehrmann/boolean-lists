1e854a17d5f4ff6dd90e38594b4a0d6b9c555371
Philipp Fehrmann <philipp-09@live.de>
2018-11-04 22:59:44 +0100

    Remove error handler and rely on the default handler

--- backend/src/api/Playlists.ts
+++ backend/src/api/Playlists.ts
@@ -8,20 +8,16 @@ const router: Express.Router = Express.Router();
 router.get("/", async (req, res) => {
     logger.info("Get api from request...");
     const api = new InitializedSpotifyApi((req as any).api);
-    try {
-        if (req.query.uri) {
-            const rawPlaylist = await Playlist.fromSpotifyUri(api, await (await api.me()).id(), req.query.uri);
-            res.send({
-                id: rawPlaylist.id(),
-                image: rawPlaylist.image(),
-                name: rawPlaylist.name(),
-                userId: rawPlaylist.userId(),
-            });
-        } else {
-            res.sendStatus(404);
-        }
-    } catch (error) {
-        res.sendStatus(500);
+    if (req.query.uri) {
+        const rawPlaylist = await Playlist.fromSpotifyUri(api, await (await api.me()).id(), req.query.uri);
+        res.send({
+            id: rawPlaylist.id(),
+            image: rawPlaylist.image(),
+            name: rawPlaylist.name(),
+            userId: rawPlaylist.userId(),
+        });
+    } else {
+        res.sendStatus(404);
     }
 });
 
