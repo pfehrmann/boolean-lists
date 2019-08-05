0e598d12ba3e38ed886e68a9e583c9b7cac1daa3
Philipp Fehrmann <philipp-09@live.de>
2019-08-05 15:40:33 +0200

    Update artist model

--- backend/src/model/spotify/Artist.ts
+++ backend/src/model/spotify/Artist.ts
@@ -1,9 +1,14 @@
+import * as logger from "winston";
 import {sleep} from "../../util";
 import {InitializedSpotifyApi} from "./SpotifyApi";
+import {Track} from "./Track";
+const Cache = require("async-disk-cache");
+
+const artistCache = new Cache("artist-cache");
 
 export class Artist {
-    public static async fromSpotifyUri(api: InitializedSpotifyApi, username: string, id: string): Promise<Artist> {
-        const response = await api.spotifyApi.getArtist(username, id);
+    public static async fromSpotifyUri(api: InitializedSpotifyApi, id: string): Promise<Artist> {
+        const response = await api.spotifyApi.getArtist(id);
         await sleep(10);
         return new Artist(api, response.body);
     }
@@ -22,5 +27,44 @@ export class Artist {
 
     public id(): string {
         return this.artist.id;
+    }
+
+    public image() {
+        return {
+            height: this.artist.images[0].height,
+            url: this.artist.images[0].url,
+            width: this.artist.images[0].width,
+        };
+    }
+
+    public async topTracks(): Promise<Track[]> {
+        if (!await artistCache.has(this.id())) {
+            logger.debug(`Fetching top tracks for artist ${this.id()} (${this.name()})`);
+            let rawTracks;
+            let tracks;
+            try {
+                rawTracks = await this.api.spotifyApi.getArtistTopTracks(this.id(), "DE");
+                tracks = rawTracks.map((rawTrack) => new Track(rawTrack, this.api));
+                await this.setTopTracks(tracks);
+            } catch (e) {
+                logger.info(`Failed to load top tracks of artist with id ${this.id()} from API`);
+                logger.error(e);
+            }
+            return tracks;
+        }
+
+        logger.debug(`Using cached songs for artist ${this.id()}`);
+        const cacheResponse = await artistCache.get(this.id());
+        try {
+            const values = JSON.parse(cacheResponse.value);
+            return values.map((value: any) => new Track(value.track, this.api));
+        } catch (e) {
+            logger.info(`Failed to load top tracks of artist with id ${this.id()}`);
+            logger.error(e);
+        }
+    }
+
+    private async setTopTracks(tracks: Track[]) {
+        await artistCache.set(this.id(), JSON.stringify(tracks));
     }
 }
