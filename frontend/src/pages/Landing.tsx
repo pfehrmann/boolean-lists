676387049acbd8fa9b59991c88d3aa44ec512223
Philipp Fehrmann <philipp.fehrmann@gmx.de>
2022-12-28 23:21:13 +0100

    Refactor landing page to look like before

--- frontend/src/pages/Landing.tsx
+++ frontend/src/pages/Landing.tsx
@@ -23,19 +23,16 @@ const styles = {
 };
 
 const _Landing = () => {
-  console.log(concert);
   return (
     <div>
       <Parallax bgImage={concert} strength={500}>
         <div style={{ minHeight: 400 }}>
-          <Grid
-            container={true}
-            style={{ marginTop: "50px" }}
-            rowSpacing={"center"}
-          >
-            <Grid item={true} xs={11} md={8}>
-              <Typography variant="h3">BooleanLists</Typography>
-              <Typography variant="h4">
+          <Grid container style={{ marginTop: "50px" }} justifyContent="center">
+            <Grid item xs={11} md={8}>
+              <Typography variant="h1" color="textSecondary">
+                BooleanLists
+              </Typography>
+              <Typography variant="h2" color="textSecondary">
                 Supercharge your Spotify playlists with ease!
               </Typography>
             </Grid>
@@ -43,73 +40,55 @@ const _Landing = () => {
         </div>
       </Parallax>
       <Parallax bgImage={turntables} strength={500} style={{ padding: 24 }}>
-        <Grid container={true} rowSpacing={"center"} spacing={24}>
-          <Grid item={true} xs={8} lg={4}>
-            <Card>
-              <CardContent>
-                <Typography variant="body1">Easy to use</Typography>
-                <br />
-                <Typography variant="body2">
-                  We have created a super simple to use graph and node based
-                  editor to make editing a breeze
-                </Typography>
-              </CardContent>
-            </Card>
-          </Grid>
-          <Grid item={true} xs={8} lg={4}>
-            <Card>
-              <CardContent>
-                <Typography variant="body1">Store your lists</Typography>
-                <br />
-                <Typography variant="body2">
-                  Yeah, we know, it's a pretty basic feature. But whatever. You
-                  can do it :D
-                </Typography>
-              </CardContent>
-            </Card>
-          </Grid>
-          <Grid item={true} xs={8} lg={4}>
-            <Card>
-              <CardContent>
-                <Typography variant="body1">Free</Typography>
-                <br />
-                <Typography variant="body2">
-                  Yep, you have heard right: This service is completely free, so
-                  no worries about any costs.
-                </Typography>
-              </CardContent>
-            </Card>
-          </Grid>
-          <Grid item={true} xs={8} lg={4}>
-            <Card>
-              <CardContent>
-                <Typography variant="body1">Beta</Typography>
-                <br />
-                <Typography variant="body2">
-                  This project is absolutely not done by now, so your data might
-                  be lost, playlists may not synchronize correctly, and so on.
-                  Be aware of this.
-                </Typography>
-              </CardContent>
-            </Card>
-          </Grid>
-          <Grid item={true} xs={8} lg={4}>
-            <Card>
-              <CardContent>
-                <Typography variant="body1">Questions?</Typography>
-                <br />
-                <Typography variant="body2">
-                  In case you have any questions, feel free to write an email to{" "}
-                  <a href={"mailto:philipp.fehrmann@gmx.de"}>
-                    philipp.fehrmann@gmx.de
-                  </a>
-                </Typography>
-              </CardContent>
-            </Card>
-          </Grid>
+        <Grid container={true} justifyContent="center" spacing={2}>
+          <FeatureCard title="Easy to use">
+            We have created a super simple to use graph and node based editor to
+            make editing a breeze
+          </FeatureCard>
+          <FeatureCard title="Store your lists">
+            Yeah, we know, it's a pretty basic feature. But whatever. You can do
+            it :D
+          </FeatureCard>
+          <FeatureCard title="Free">
+            Yep, you have heard right: This service is completely free, so no
+            worries about any costs.
+          </FeatureCard>
+          <FeatureCard title="Beta">
+            This project is absolutely not done by now, so your data might be
+            lost, playlists may not synchronize correctly, and so on. Be aware
+            of this.
+          </FeatureCard>
+          <FeatureCard title="Questions?">
+            In case you have any questions, feel free to write an email to{" "}
+            <a href={"mailto:philipp.fehrmann@gmx.de"}>
+              philipp.fehrmann@gmx.de
+            </a>
+          </FeatureCard>
         </Grid>
       </Parallax>
     </div>
+  );
+};
+
+interface FeatureCardProps {
+  title: string;
+  children: string | React.ReactNode;
+}
+const FeatureCard = ({ title, children }: FeatureCardProps) => {
+  return (
+    <Grid item={true} xs={8} lg={4}>
+      <Card>
+        <CardContent>
+          <Typography variant="h3" color="textSecondary">
+            {title}
+          </Typography>
+          <br />
+          <Typography variant="h4" color="textSecondary">
+            {children}
+          </Typography>
+        </CardContent>
+      </Card>
+    </Grid>
   );
 };
 
