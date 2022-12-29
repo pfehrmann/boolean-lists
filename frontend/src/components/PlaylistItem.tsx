94d822aa7c6eb674304940bb7b2f7fc400908d63
Philipp Fehrmann <philipp.fehrmann@gmx.de>
2022-12-29 01:24:33 +0100

    Fix icon

--- frontend/src/components/PlaylistItem.tsx
+++ frontend/src/components/PlaylistItem.tsx
@@ -9,7 +9,7 @@ import * as React from "react";
 import { Link } from "react-router-dom";
 import { Playlist } from "../api";
 
-import * as listIcon from "./list_icon.svg";
+import listIcon from "./list_icon.svg";
 
 import { withStyles } from "@mui/styles";
 import Typography from "@mui/material/Typography";
