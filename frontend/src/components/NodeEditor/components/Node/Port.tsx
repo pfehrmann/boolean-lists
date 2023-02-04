78aff622dfad4f9975b0005123a89fcb76fa15ea
Philipp Fehrmann <philipp.fehrmann@gmx.de>
2023-02-03 22:37:56 -0500

    Use drag context in ports

--- frontend/src/components/NodeEditor/components/Node/Port.tsx
+++ frontend/src/components/NodeEditor/components/Node/Port.tsx
@@ -1,6 +1,8 @@
 import { styled } from '@mui/material';
-import React from 'react';
+import React, { useContext, useRef } from 'react';
 import { PropsWithChildren } from 'react';
+
+import { DragContext } from '../DragRoot';
 
 const StyledPort = styled('div')<{ order: 'start' | 'end' }>`
   display: flex;
@@ -20,9 +22,33 @@ const Ball = styled('div')`
 export const Port = ({
   children,
   ball,
-}: PropsWithChildren<{ ball: 'start' | 'end' }>) => (
-  <StyledPort order={ball}>
-    <Ball />
-    {children}
-  </StyledPort>
-);
+}: PropsWithChildren<{ ball: 'start' | 'end' }>) => {
+  const { setDraggingElements } = useContext(DragContext);
+  const ref = useRef<HTMLDivElement | null>(null);
+  console.log(ref);
+
+  return (
+    <StyledPort
+      order={ball}
+      onMouseDown={(event) => {
+        event.preventDefault();
+        event.stopPropagation();
+        console.log('registered element for movement');
+        setDraggingElements([
+          {
+            onMove: (x, y) => {
+              console.log(x, y);
+            },
+          },
+        ]);
+      }}
+    >
+      <div style={{ position: 'relative' }}>
+        <Ball />
+        {/* start of a line, the line and the end of a line. 
+        Once dropped, will need to have a ref to both elements and then draw the line there. */}
+      </div>
+      {children}
+    </StyledPort>
+  );
+};
